5e12ebdbedaa8bebb5b71ae987c045dccec8b164
Fadhilabie <fadhilmuhhab@gmail.com>
2023-10-01 14:33:58 +0700

    p

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Swiper
+var swiper = new Swiper(".mySwiperPopuler", {
+    grabCursor: true,
+    centeredSlides: false,
+    spaceBetween: 20,
+    slidesPerView: "auto",
+    navigation: {
+      nextEl: ".fa-circle-chevron-right",
+      prevEl: ".fa-circle-chevron-left",
+    },
+  });
